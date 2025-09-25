dd303295079df141bc55787c3629d452b4cb9b9e
João Vitor Rosado <joaovrosado2003@gmail.com>
2025-09-25 11:36:00 -0300

    Adicionando o creat ao banco apos fazer o envio do formulario (Mensagem de confirmação não funcionou)

--- DesafioWeb/Controllers/FundacoesController.cs
+++ DesafioWeb/Controllers/FundacoesController.cs
@@ -1,19 +1,73 @@
+using DesafioWeb.Data;
+using DesafioWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DesafioWeb.Controllers
 {
     public class FundacoesController : Controller
     {
-        // Método que vai retornar a view Index.cshtml
-        public IActionResult Index()
+        private readonly Database _database;
+
+        public FundacoesController()
         {
-            return View(); // procura automaticamente Views/Fundacoes/Index.cshtml
+            _database = new Database();
+            _database.CriarTabela(); // garante que a tabela exista
         }
 
+        // GET: Fundacoes/Index
+        public IActionResult Index()
+        {
+            // opcional: listar todas as fundações
+            var lista = _database.ListarTodas();
+            return View(lista); // envia a lista para a view Index.cshtml
+        }
+
+        // GET: Fundacoes/Create
+        public IActionResult Create()
+        {
+            return View(); // apenas exibe o formulário
+        }
+
+        // POST: Fundacoes/Create
+        [HttpPost]
+        public IActionResult Create(Fundacao fundacao)
+        {
+            try
+            {
+                var existente = _database.BuscarPorCNPJ(fundacao.CNPJ);
+                if (existente != null)
+                {
+                    ViewBag.Mensagem = "CNPJ já cadastrado!";
+                    return View();
+                }
+
+                _database.Inserir(fundacao);
+                ViewBag.Mensagem = "Cadastro efetuado com sucesso!";
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Mensagem = "Erro ao cadastrar: " + ex.Message;
+                return View();
+            }
+        }
+
+        // GET: Fundacoes/Edit
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        // GET: Fundacoes/Delete
         public IActionResult Delete()
         {
             return View();
         }
 
+        // GET: Fundacoes/Search
+        public IActionResult Search()
+        {
+            return View();
+        }
     }
 }
